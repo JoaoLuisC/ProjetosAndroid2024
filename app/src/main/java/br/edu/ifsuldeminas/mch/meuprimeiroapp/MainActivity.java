f47fb1859d70e4e670d029ff68350163b3acf055
João Luis Carodoso <joao.lc12@hotmail.com>
2024-04-09 09:09:37 -0300

    aula 09-04

--- app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/MainActivity.java
@@ -2,13 +2,69 @@ package br.edu.ifsuldeminas.mch.meuprimeiroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private EditText userName;
+    private EditText userPW;
+    private static String USER = "joao";
+    private static String PW = "123";
+    private static final String TAG = "login_main_activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Acessar elementos
+        buttonLogin = findViewById(R.id.buttonLogin);
+        userName = findViewById(R.id.editTextUser);
+        userPW = findViewById(R.id.editTextNumberPassword);
+
+        //Chamar o onClickListener
+        buttonLogin.setOnClickListener(view -> {
+
+            String userNameStr = userName.getText().toString();
+            String userPWStr = userPW.getText().toString();
+
+            if(userNameStr.equals("")) {
+                Toast toast = Toast.makeText(view.getContext(), R.string.login_user_name_empty, Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+
+            if(userPWStr.equals("")) {
+                Toast toast = Toast.makeText(view.getContext(), R.string.login_user_pw_empty, Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+
+            if(userNameStr.equals(USER) && userPWStr.equals(PW)) {
+                //Abrir uma Tela/Activity (Welcome)
+                Intent welcomeIntent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                welcomeIntent.putExtra("user_name", userNameStr);
+                startActivity(welcomeIntent);
+
+            }else {
+                Toast toast = Toast.makeText(view.getContext(), R.string.login_failure, Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
     }
+
+    // Forma depreciada de implementação
+    public void forgotPW (View view) {
+        Toast toast = Toast.makeText(view.getContext(), R.string.login_button_forgot_pw_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
--- app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/WelcomeActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/meuprimeiroapp/WelcomeActivity.java
@@ -1,2 +1,59 @@
-package br.edu.ifsuldeminas.mch.meuprimeiroapp;public class WelcomeActivity {
+package br.edu.ifsuldeminas.mch.meuprimeiroapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Registrar o layout
+        setContentView(R.layout.activity_welcome);
+
+        Intent intentDaChamada = getIntent();
+        String userName = intentDaChamada.getStringExtra("user_name");
+
+        View layout = findViewById(R.id.activity_welcome_id);
+
+        Snackbar snackbar = Snackbar.make(layout, "Bem vindo" + userName, Snackbar.LENGTH_LONG);
+
+        snackbar.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
